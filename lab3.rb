cfaefb75902ca37785a978a64ecc01ebec281e12
raulp93 <raulp93@gmail.com>
2025-05-09 21:58:22 -0700

    added initial files

---
+++ lab3.rb
@@ -0,0 +1,29 @@
+#require_relative "element"   # uncomment to load element.rb
+#require_relative "player"    # uncomment to load player.rb 
+#require_relative "history"   # uncomment to load history.rb
+
+#####################################
+#     CS 381 - Programming Lab #3		#
+#																		#
+#  Raul Preciado                    #
+#  preciadr@oregonstate.edu         #
+#										                #
+#####################################
+
+
+
+# your code here
+
+
+
+def game(rounds)
+
+	# your code here
+	
+end
+
+
+# Main Program (should be last)
+n_rounds = 5
+# call to kick off the game
+game(n_rounds)
