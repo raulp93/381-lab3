737d23b6fdebc0747022fc7a632328007b26b6e8
raulp93 <raulp93@gmail.com>
2025-05-12 10:09:50 -0700

    added history class which handles the records of each round

---
+++ history.rb
@@ -0,0 +1,22 @@
+
+class History
+  attr_accessor :@plays, :@opponent_plays, :@score
+  def initialize
+    @score = 0
+    @opponent_plays = Array.new
+    @plays = Array.new
+  end
+
+  def log_play(move)
+    @plays.push(move)
+  end
+
+  def log_opponent_play(move)
+    @opponent_plays.push(move)
+  end
+
+  def add_score
+    @score += 1
+  end
+
+end
